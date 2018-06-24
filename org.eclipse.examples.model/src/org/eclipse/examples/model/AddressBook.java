28f95f71a7df58c17a788b79b878565cd4bfa66a
Suraj <Suraj@Suraj-PC>
2018-06-24 22:14:52 +0530

    Ui model Commit

---
+++ org.eclipse.examples.model/src/org/eclipse/examples/model/AddressBook.java
@@ -0,0 +1,18 @@
+package org.eclipse.examples.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook implements Serializable{
+	
+	List<Group> groups;
+	
+	public List<Group> getGroups() {
+		if(groups == null) {
+			groups = new ArrayList<>();
+		}
+		return groups;
+	}
+
+}
---
+++ org.eclipse.examples.model/src/org/eclipse/examples/model/ModelManager.java
@@ -0,0 +1,77 @@
+package org.eclipse.examples.model;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class ModelManager {
+	
+	private static ModelManager manager;
+	private AddressBook addressBook;
+	
+	private ModelManager() {
+		
+	}
+	
+	public static ModelManager getInstance() {
+		if(manager == null) {
+			manager = new ModelManager();
+		}
+		return manager;
+	}
+	
+	public AddressBook getAddressBook() {
+		if(addressBook == null) {
+			addressBook = new AddressBook();
+			//Create Group
+			Group group = new Group();
+			group.setName("Official");
+			addressBook.getGroups().add(group);
+			//Create Contact
+			Contact contact = new Contact();
+			contact.setName("ABC Sharma");
+			contact.setAddress("MG Road, Bangalore");
+			group.getContacts().add(contact);
+		}
+		return addressBook;
+	}
+	
+	public void save(String fileName) {
+		try {
+			FileOutputStream fos = new FileOutputStream(fileName);
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(addressBook);
+			oos.close();
+			fos.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void load(String fileName) {
+		try {
+			FileInputStream fis = new FileInputStream(fileName);
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			addressBook = (AddressBook)ois.readObject();
+			ois.close();
+			fis.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
